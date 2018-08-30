c0eb3248d871b56779935e144fed3aa68fff2c3b
k0oAcevn <k0olano0.ltd@gmail.com>
2018-08-30 10:41:07 +0700

    Clear our existing models on load to avoid conficts when reloading data

--- planing-pilot/src/app/reducers/entitiesReducer.js
+++ planing-pilot/src/app/reducers/entitiesReducer.js
@@ -10,6 +10,14 @@ import schema from "app/schema"
     // Get a reference to the correct version of model classes for this Session
     const {Pilot, MechDesign, Mech} = session;
     const {pilots, designs, mechs} = payload;
+
+    //Clear out any existing models from state so that we can avoid
+    //conflicts from the new data coming in if data is reloaded
+    [Pilot, Mech, MechDesign].forEach(modelType => {
+        modelType.all().withModels.forEach(model => model.delete());
+        session.state = session.reduce();
+    });
+
     // Queue up creation commands for each pilot entry
     pilots.forEach(pilot => Pilot.parse(pilot));
     designs.forEach(design => MechDesign.parse(design));
