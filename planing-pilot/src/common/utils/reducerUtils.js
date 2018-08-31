a24b595a45e364c78a51f35f796d06699ee8050f
k0oAcevn <k0olano0.ltd@gmail.com>
2018-08-31 13:34:13 +0700

    Add createConditionalSliceReducer utility

--- planing-pilot/src/common/utils/reducerUtils.js
+++ planing-pilot/src/common/utils/reducerUtils.js
@@ -6,10 +6,31 @@ export function createReducer(initialState, fnMap) {
     };
 }
 
-export function reduceReducers(...reducers) {
+export function reduceReducer(...reducers) {
     return (previous, current) =>
         reducers.reduce(
             (p, r) => r(p, current),
             previous
         );
+}
+
+
+export function createConditionalSliceReducer(sliceName, fnMap) {
+    //Create a reducer that knows how to handle one slice of state, with these action types
+    const sliceReducer = createReducer({}, fnMap);
+
+    //Create a new wrapping reducer
+    return (state, action) => {
+        //Check to see if this slice reducer knows how to handle this action
+        if(fnMap[action.type]){
+            //If it does, pass the slice to the slice reducer, and update slice
+            return {
+                ...state,
+                [sliceName] : sliceReducer(state[sliceName], action),
+            };
+        }
+
+        //Otherwise, return the existing state unchanged
+        return state;
+    }
 }
