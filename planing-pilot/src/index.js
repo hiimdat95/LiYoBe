4dcd29ded1a89fa31fc3806658c43ae36d252cee
k0oAcevn <k0olano0.ltd@gmail.com>
2018-10-02 16:56:54 +0700

    Update app dependencies to current versions

--- planing-pilot/src/index.js
+++ planing-pilot/src/index.js
@@ -25,27 +25,6 @@ let render = () => {
 };
 if(module.hot){
     //support hot reloading of components
-    //and display an overlay for runtime errors
-    const renderApp=render;
-    const renderError= (error)=>{
-        const RedBox = require("redbox-react").default;
-        ReactDOM.render(
-            <RedBox error={error} />,
-            rootEl,
-        );
-    };
-    //In development, we wrap the rendering function to catch errors,
-    //and if something breaks, log the error and render it to the screen
-    render = () => {
-        try{
-            renderApp();
-        }
-        catch(error){
-            console.log(error);
-            renderError(error);
-        }
-    };
-
     //Whenever the App component file or one of its dependencies
     //is changed, re-import the updated component and re-render it
     module.hot.accept("./app/layout/App",()=>{
