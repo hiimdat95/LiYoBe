cbcaf151e3f3f8836f3ec27d488fcd2f31f73486
k0oAcevn <k0olano0.ltd@gmail.com>
2018-10-09 12:00:38 +0700

    Show current unit detail values in the Unit TOE tree

--- planing-pilot/src/features/unitInfo/UnitOrganizationTree/Lance.jsx
+++ planing-pilot/src/features/unitInfo/UnitOrganizationTree/Lance.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
+
 import {
     List,
 } from "semantic-ui-react";
-import { getEntitiesSession } from "features/entities/entitySelectors";
+
+import {getEntitiesSession} from "features/entities/entitySelectors";
+
 import LancePilot from "./LancePilot";
+
+
 const mapState = (state, ownProps) => {
     const session = getEntitiesSession(state);
-    const { Lance } = session;
+    const {Lance} = session;
+
     let lance, pilots;
-    if (Lance.hasId(ownProps.lanceID)) {
+
+    if(Lance.hasId(ownProps.lanceID)) {
         const lanceModel = Lance.withId(ownProps.lanceID);
+
         lance = lanceModel.ref;
         pilots = lanceModel.pilots.toRefArray().map(pilot => pilot.id);
     }
-    return { lance, pilots };
+
+    return {lance, pilots};
 };
-const UNKNOWN_LANCE = { name: "Unknown" }
-const Lance = ({ lance = UNKNOWN_LANCE, pilots = [] }) => {
-    const { name } = lance;
+
+const UNKNOWN_LANCE =  {name : "Unknown"}
+
+const Lance = ({lance = UNKNOWN_LANCE, pilots = []}) => {
+    const {name} = lance;
+
     const lancePilots = pilots.map(pilotID => <LancePilot key={pilotID} pilotID={pilotID} />);
+
     return (
         <List.Item>
             <List.Icon name="cube" />
@@ -32,4 +45,6 @@ const Lance = ({ lance = UNKNOWN_LANCE, pilots = [] }) => {
         </List.Item>
     )
 };
-export default connect(mapState)(Lance); 
+
+
+export default connect(mapState)(Lance);
--- planing-pilot/src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
+++ planing-pilot/src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
@@ -1,33 +1,60 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 
 import {
     List,
 } from "semantic-ui-react";
-import { getEntitiesSession } from "features/entities/entitySelectors";
+
+import {getEntitiesSession} from "features/entities/entitySelectors";
 
 import Lance from "./Lance";
 
 const mapState = (state) => {
     const session = getEntitiesSession(state);
-    const { Unit } = session;
+    const {Unit} = session;
 
-    let lances;
+    let unit, faction, lances;
 
     const unitModel = Unit.all().first();
-    if (unitModel) {
+
+    if(unitModel) {
+        unit = unitModel.ref;
+        faction = unitModel.affiliation.ref;
         lances = unitModel.lances.toRefArray().map(lance => lance.id);
     }
-    return { lances };
+
+    return {unit, faction, lances};
 }
-const UnitOrganizationTree = ({ lances = [] }) => {
+
+const UNKNOWN_UNIT = {name : "Unknown"};
+
+
+
+const UnitOrganizationTree = ({unit = UNKNOWN_UNIT, faction = {}, lances = []}) => {
+    const {name, color} = unit;
+    const {name : factionName} = faction;
+
+    const colorBlock = <div
+        style={{
+            marginLeft : 10,
+            backgroundColor : color,
+            border : "1px solid black",
+            height : 20,
+            width : 40,
+        }}
+    />;
+
+    const displayText = factionName ? `${name} / ${factionName}` : name;
+
+
     const lanceEntries = lances.map(lanceID => <Lance key={lanceID} lanceID={lanceID} />);
+
     return (
         <List size="large">
             <List.Item>
                 <List.Icon name="cubes" />
                 <List.Content>
-                    <List.Header>Black Widow Company</List.Header>
+                    <List.Header style={{display : "flex"}}>{displayText} {colorBlock}</List.Header>
                     <List.List>
                         {lanceEntries}
                     </List.List>
