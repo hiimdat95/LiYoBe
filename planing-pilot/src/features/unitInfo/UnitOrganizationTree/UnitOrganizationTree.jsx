19e748e8ba2edc992299ade47a45900027a2f810
k0oAcevn <k0olano0.ltd@gmail.com>
2018-10-09 11:46:47 +0700

    Show lances in the Unit TOE tree based on current Unit entry contents

--- planing-pilot/src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
+++ planing-pilot/src/features/unitInfo/UnitOrganizationTree/UnitOrganizationTree.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import {
     List,
 } from "semantic-ui-react";
+import { getEntitiesSession } from "features/entities/entitySelectors";
 
-import LancePilot from "./LancePilot";
+import Lance from "./Lance";
 
+const mapState = (state) => {
+    const session = getEntitiesSession(state);
+    const { Unit } = session;
 
-const UnitOrganizationTree = () => {
+    let lances;
+
+    const unitModel = Unit.all().first();
+    if (unitModel) {
+        lances = unitModel.lances.toRefArray().map(lance => lance.id);
+    }
+    return { lances };
+}
+const UnitOrganizationTree = ({ lances = [] }) => {
+    const lanceEntries = lances.map(lanceID => <Lance key={lanceID} lanceID={lanceID} />);
     return (
         <List size="large">
             <List.Item>
@@ -15,44 +29,7 @@ const UnitOrganizationTree = () => {
                 <List.Content>
                     <List.Header>Black Widow Company</List.Header>
                     <List.List>
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Command Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={1} />
-                                    <LancePilot pilotID={2} />
-                                    <LancePilot pilotID={3} />
-                                    <LancePilot pilotID={4} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Fire Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={5} />
-                                    <LancePilot pilotID={6} />
-                                    <LancePilot pilotID={7} />
-                                    <LancePilot pilotID={8} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
-
-                        <List.Item>
-                            <List.Icon name="cube" />
-                            <List.Content>
-                                <List.Header>Recon Lance</List.Header>
-                                <List.List>
-                                    <LancePilot pilotID={9} />
-                                    <LancePilot pilotID={10} />
-                                    <LancePilot pilotID={11} />
-                                    <LancePilot pilotID={12} />
-                                </List.List>
-                            </List.Content>
-                        </List.Item>
+                        {lanceEntries}
                     </List.List>
                 </List.Content>
             </List.Item>
@@ -60,4 +37,4 @@ const UnitOrganizationTree = () => {
     )
 }
 
-export default UnitOrganizationTree;
+export default connect(mapState)(UnitOrganizationTree);
